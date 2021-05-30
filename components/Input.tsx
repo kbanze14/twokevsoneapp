80d820d6a23b8f97d5b80aab2fa66a0d91aab63a
kbanze14 <k.banze@setonhill.edu>
2021-05-30 19:39:35 -0400

    bilf

---
+++ components/Input.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, Modal, Button, TextInput, StyleSheet } from 'react-native';
+
+export default function ListInput () {
+    const [item, setItem] = React.useState('Milk')
+    return(
+        <View>
+        <TextInput> </TextInput>
+        <Button title = 'Add'
+        onPress = {() => {setItem(item)}}> 
+        </Button>
+        </View>
+    );
+}
--- components/Journal.tsx
+++ components/Journal.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, Modal, Button, TextInput, StyleSheet } from 'react-native';
+import ListInput from '../components/Input';
 
 export default function Home () {
     const [showModal, setShowModal] = React.useState(false);
@@ -15,9 +16,9 @@ export default function Home () {
                 <View style={styles.container}>
                     <Button title = 'hide modal'
                     onPress = {() => {setShowModal(!showModal)}}>
-
                     </Button>
                     <Text style= {styles.text}> List </Text>
+                    <ListInput/>
                 </View>
             </Modal>
             <Button title = 'show modal'
@@ -31,10 +32,11 @@ export default function Home () {
 const styles = StyleSheet.create({
 container:{
     paddingTop: 10, 
+    alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: 'red',
     height:200,
-    width: 100,
+    width: 430,
 },
 text:{
     fontSize: 20,
